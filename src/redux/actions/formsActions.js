e2abe86576e9b93635152bf75893ce089a721a76
Kate <kate@MacBook-Pro-avens.local>
2023-11-21 18:28:35 +0300

    fix

--- src/redux/actions/formsActions.js
+++ src/redux/actions/formsActions.js
@@ -1,6 +1,5 @@
 import { IS_SUBMITED_FORM } from '../actionTypes';
 
-// eslint-disable-next-line import/prefer-default-export
 export const isSubmitedForm = (isSubmited) => {
   return {
     type: IS_SUBMITED_FORM,
